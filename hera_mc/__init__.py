3ebed649a59dcd2caad1732064ca752715c5a2ea
Peter Williams <peter@newton.cx>
2016-10-10 18:35:03 -0400

    hera_mc/host_status.py: add code for M&C logging of computer status

--- hera_mc/__init__.py
+++ hera_mc/__init__.py
@@ -15,5 +15,10 @@ logger = logging.getLogger(__name__)
 
 # Now we can pull in the rest of our definitions.
 
+def NotNull (kind, **kwargs):
+    from sqlalchemy import Column
+    return Column (kind, nullable=False, **kwargs)
+
 from .version import __version__
+from . import host_status
 from . import mc
