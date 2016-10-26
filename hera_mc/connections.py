8430f7945eab2b58f5400b214f872aa940442c7a
David DeBoer <ddeboer@berkeley.edu>
2016-10-26 09:59:53 -0700

    first working-ish version of connections

---
+++ hera_mc/connections.py
@@ -0,0 +1,66 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2016 the HERA Collaboration
+# Licensed under the 2-clause BSD license.
+
+"""M&C logging of the parts and the connections between them.
+
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import datetime
+import os
+import socket
+
+import numpy as np
+
+from sqlalchemy import BigInteger, Column, DateTime, Float, Integer, String, func
+
+from . import MCDeclarativeBase, NotNull
+
+
+class Parts(MCDeclarativeBase):
+    """A table logging parts within the HERA system
+
+    """
+    __tablename__ = 'parts'
+
+    hpn = Column(String(64), primary_key=True)
+    "A unique HERA part number for each record; intend to QRcode with this string."
+
+    kind = NotNull(String(64))
+    "A part-dependent string, i.e. feed, frontend, ..."
+
+    manufacture_date = NotNull(DateTime)
+    "The date when the part was manufactured (or assigned by project)."
+
+    def __repr__(self):
+        return '<heraPartNumber id={self.hpn} kind={self.kind} manufacture_date={self.manufacture_date}'.format(self=self)
+
+class Connections(MCDeclarativeBase):
+    """A table for logging connections between part
+
+    """
+
+    __tablename__ = 'connections'
+
+    id = Column(BigInteger, primary_key=True)
+    "A unique identifier to key on connections"
+
+    A = NotNull(String(64))
+    "A refers to the skyward part, e.g. frontend:cable, 'A' is the frontend, 'B' is the cable.  Signal flows from A->B"
+
+    B = NotNull(String(64))
+    "B refers to the part that is further from the sky, e.g. "
+
+    port_A = NotNull(Integer)
+    "port_A refers to the port on the part that is skyward"
+
+    port_B = NotNull(Integer)
+    "port_B refers to the port on the part that is further from the sky"
+    
+    start_time = NotNull(DateTime)
+    "start_time is the time that the connection is set"
+
+    stop_time = Column(DateTime)
+    "stop_time is the time that the connection is removed"
---
+++ hera_mc/tests/test_connections.py
@@ -0,0 +1,36 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2016 the HERA Collaboration
+# Licensed under the 2-clause BSD license.
+
+"""Testing for `hera_mc.connections`.
+
+I don't think there's really much to do here besides just test insertion of a
+record.
+
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import unittest
+
+from hera_mc import connections, mc
+
+
+class test_connections(unittest.TestCase):
+    def setUp(self):
+        self.test_db = mc.connect_to_mc_testing_db()
+        self.test_db.create_tables()
+        self.test_conn = self.test_db.engine.connect()
+        self.test_trans = self.test_conn.begin()
+        self.test_session = mc.MCSession(bind=self.test_conn)
+
+    def tearDown(self):
+        self.test_trans.rollback()
+        self.test_conn.close()
+        self.test_db.drop_tables()
+
+    def test_add_part(self):
+        self.test_session.add(connections.Parts())
+
+if __name__ == '__main__':
+    unittest.main()
