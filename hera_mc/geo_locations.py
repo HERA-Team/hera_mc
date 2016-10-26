78964dd35c551fef7df67a52ec6980c53fe01033
David DeBoer <ddeboer@berkeley.edu>
2016-10-26 14:54:20 -0700

    defined geo_locations class

--- hera_mc/geo_locations.py
+++ hera_mc/geo_locations.py
@@ -14,7 +14,7 @@ import socket
 
 import numpy as np
 
-from sqlalchemy import BigInteger, Column, DateTime, Float, ForeignKey, Integer, String, func
+from sqlalchemy import Column, Float, Integer, String, func
 
 from . import MCDeclarativeBase, NotNull
 
@@ -26,21 +26,29 @@ class GeoLocations(MCDeclarativeBase):
     __tablename__ = 'geo_locations'
 
     station_name = Column(String(64), primary_key=True)
+    "Colloquial name of station (which is a unique location on the ground.  This is the primary key, so precision matters."
 
     station_number = Column(Integer)
+    "Unique station number that the correlator and MIRIAD want.  Currently set to numbers as of 16/10/26.  This will be superseded by future version."
 
     future_station_number = Column(Integer)
+    "Unique station nymber in the HERA era."
 
     datum = Column(String(64))
+    "Datum of the geoid."
 
     tile = Column(String(64))
+    "UTM tile"
 
     northing = Column(Float(precision='53'))
+    "Northing coordinate in m"
 
     easting = Column(Float(precision='53'))
+    "Easting coordinate in m"
 
     elevation = Column(Float)
+    "Elevation in m"
 
     def __repr__(self):
-        return '<heraPartNumber id={self.hpn} kind={self.kind} manufacture_date={self.manufacture_date}'.format(self=self)
+        return '<station_name={self.station_name} station_number={self.station_number} northing={self.northing} easting={self.easting} elevation={self.elevation}'.format(self=self)
 
