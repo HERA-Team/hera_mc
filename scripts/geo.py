dcb985484689e0f278bb16d2f5dad4b9092c01ab
David DeBoer <ddeboer@berkeley.edu>
2016-10-29 11:14:52 -0700

    updated args for geo

--- scripts/geo.py
+++ scripts/geo.py
@@ -33,23 +33,24 @@ def split_arrays(args):
                     sub_arrays[sad].append(a.station_name)
     return sub_arrays
 
-def plot_arrays(args, sub_arrays):
+def plot_arrays(args, sub_arrays, overplot=None):
     """Plot the various sub-array types"""
     markers = {'HH':'ro','PH':'rs','PI':'gs','PP':'bd','S':'bs'}
-    plt.figure(args.graph)
+    vpos = {'E':0,'N':1,'Z':2}
+    plt.figure(args.xgraph+args.ygraph)
     db = mc.connect_to_mc_db(args)
     with db.sessionmaker() as session:
         for key in sub_arrays.keys():
             for loc in sub_arrays[key]:
                 for a in session.query(geo_location.GeoLocation).filter(geo_location.GeoLocation.station_name==loc):
-                    x = a.easting
-                    if args.graph=='XY':
-                        y = a.northing
-                    else:
-                        y = a.elevation
-                plt.plot(x,y,markers[key])
-    if args.graph=='XY':
+                    v = [a.easting,a.northing,a.elevation]
+                plt.plot(v[vpos[args.xgraph]],v[vpos[args.ygraph]],markers[key])
+    if overplot:
+        plt.plot(overplot[vpos[args.xgraph]],overplot[vpos[args.ygraph]],'ys', markersize=10,label=overplot[3])
+        plt.legend(loc='upper right')
+    if args.xgraph!='Z' and args.ygraph!='Z':
         plt.axis('equal')
+    plt.plot(xaxis=args.xgraph,yaxis=args.ygraph)
     plt.show()
 
 def locate_station(args, sub_arrays):
@@ -58,7 +59,7 @@ def locate_station(args, sub_arrays):
         station_search = int(args.locate)
         station_desig = geo_location.GeoLocation.station_number
     except ValueError:
-        if args.locate[0].lower() == 'h':
+        if args.locate[0] == 'H':
             station_search = args.locate[1:]
         else:
             station_search = args.locate
@@ -69,7 +70,8 @@ def locate_station(args, sub_arrays):
                 if a.station_name in sub_arrays[key]:
                     this_sub_array = key
                     break
-            if args.verbose:
+            v = [a.easting,a.northing,a.elevation,a.station_name]
+            if args.verbosity=='m' or args.verbose=='h':
                 print('station_name: ',a.station_name)
                 print('\tstation_number: ',a.station_number)
                 print('\teasting: ',a.easting)
@@ -78,15 +80,26 @@ def locate_station(args, sub_arrays):
                 print('\tsub-array: ',this_sub_array)
             else:
                 print(a,this_sub_array)
+    return v
 
 if __name__=='__main__':
     parser = mc.get_mc_argument_parser()
-    parser.add_argument('-g','--graph',help="Graph data:  XY or Z, default is No",default=False)
+    parser.add_argument('-g','--graph',help="Graph data of all elements",action='store_true')
+    parser.add_argument('-s','--show',help='Graph and locate a station (same as geo.py -gl XX)',default=False)
     parser.add_argument('-l','--locate',help="Print out location of given station_name. Prepend with 'h' for HH station_name. Integer if station_number",default=False)
-    parser.add_argument('-v','--verbose',help="Set to print out long forms of things.",action="store_true")
+    parser.add_argument('-v','--verbosity',help="Set verbosity {l,m,h}.",default="m")
+    parser.add_argument('-x','--xgraph',help='X-axis of graph {N,E,Z} [E]',default='E')
+    parser.add_argument('-y','--ygraph',help='Y-axis of graph {N,E,Z} [N]',default='N')
     args = parser.parse_args()
+    args.xgraph = args.xgraph.upper()
+    args.ygraph = args.ygraph.upper()
     sub_arrays = split_arrays(args)
-    if args.graph:
-        plot_arrays(args,sub_arrays)
+    located = None
+    if args.show:
+        args.locate = args.show 
+        args.graph = True
     if args.locate:
+        args.locate = args.locate.upper()
         located = locate_station(args, sub_arrays)
+    if args.graph:
+        plot_arrays(args,sub_arrays,located)
