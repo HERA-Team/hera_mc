77ad585ac95f4ab8eece7c097a11d6a150405e9e
Bryna Hazelton <brynah@phys.washington.edu>
2016-11-15 14:54:17 -0800

    add script to write antenna position information to a csv

---
+++ scripts/write_antenna_location_file.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2016 the HERA Collaboration
+# Licensed under the 2-clause BSD license.
+
+"""
+Script to write out antenna locations for use in cal files.
+"""
+import pandas as pd
+from hera_mc import mc
+
+parser = mc.get_mc_argument_parser()
+parser.add_argument('file', help="file name to save antenna locations to")
+args = parser.parse_args()
+filename = args.file
+db = mc.connect_to_mc_db(args)
+
+# ant_locs = pd.DataFrame(columns=('station_name', 'station_number',
+#                                  'future_station_number', 'datum', 'tile',
+#                                  'northing', 'easting', 'elevation'))
+ant_locs = pd.read_sql_table('geo_location', db.engine)
+
+ant_locs.to_csv(filename, index=False)
