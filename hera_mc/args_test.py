e289ddf70b6e0f1b3e60e5d2cb351397aa751683
David DeBoer <ddeboer@berkeley.edu>
2017-05-23 14:26:48 -0700

    date-fixed args_test

--- hera_mc/args_test.py
+++ hera_mc/args_test.py
@@ -30,8 +30,8 @@ parser.add_argument('--mapr-cols', help="Specify a subset of parts to show in ma
                     dest='mapr_cols', default='all')
 parser.add_argument('--levels-testing', help="Set to test filename if correlator levels not accessible [levels.tst]", 
                     dest='levels_testing', default='levels.tst')
-parser.add_argument('--date', help="MM/DD/YY or now [now]", default='now')
-parser.add_argument('--time', help="hh:mm or now [now]", default='now')
+parser.add_argument('--date', help="UTC YYYY/MM/DD or now [now]", default='now')
+parser.add_argument('--time', help="UTC hh:mm or now [now]", default='now')
 active_group = parser.add_mutually_exclusive_group()
 active_group.add_argument('--show-active', help="Flag to show only the active parts/connections (default)",dest='active', action='store_true')
 active_group.add_argument('--show-all', help="Flag to show all parts/connections",dest='active', action='store_false')
