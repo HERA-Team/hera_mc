0ff03671293eba41cb0be994e4f68b9073079d9b
Bryna Hazelton <bryna.hazelton@gmail.com>
2017-08-15 15:04:26 -0700

    Add some testing

--- hera_mc/tests/test_geo_location.py
+++ hera_mc/tests/test_geo_location.py
@@ -10,7 +10,7 @@
 from __future__ import absolute_import, division, print_function
 
 import unittest
-
+import numpy as np
 from hera_mc import geo_location, geo_handling, mc, cm_transfer
 from hera_mc.tests import TestHERAMC
 
@@ -47,9 +47,19 @@ class TestGeo(TestHERAMC):
         cofa = self.h.cofa()
         self.assertTrue('cofa' in cofa.station_name.lower())
 
+        # test that function works the same as method
+        cofa_func = geo_handling.cofa(session=self.test_session)
+        self.assertTrue(cofa.isclose(cofa_func))
+
     def test_get_location(self):
         located = self.h.get_location([self.test_element_station_name], 'now')
         self.assertTrue(located[0].station_name == self.test_element_station_name)
+
+        # test that function works the same as method
+        located_func = geo_handling.get_location([self.test_element_station_name],
+                                                 'now', session=self.test_session)
+        for loc_i in range(len(located)):
+            self.assertTrue(located[loc_i].isclose(located_func[loc_i]))
 
     def test_update_new(self):
         nte = self.test_element_prefix + '_new_test_element'
@@ -78,6 +88,26 @@ class TestGeo(TestHERAMC):
     def test_is_in_geo_location(self):
         found_it = self.h.is_in_geo_location(self.test_element_station_name)
         self.assertTrue(found_it)
+
+    def test_correlator_info(self):
+        corr_dict = self.h.get_cminfo_correlator(cm_csv_path=mc.test_data_path)
+
+        ant_names = corr_dict['antenna_names']
+        self.assertEqual(len(ant_names), 12)
+
+        corr_inputs = corr_dict['correlator_inputs']
+
+        index = np.where(np.array(ant_names) == 'HH27')[0]
+        self.assertEqual(len(index), 1)
+        index = index[0]
+
+        self.assertEqual(corr_inputs[index], ('DF7B4', 'DF7B3'))
+
+        self.assertEqual([int(name.split('HH')[1]) for name in ant_names],
+                         corr_dict['antenna_numbers'])
+
+        self.assertEqual(set(corr_dict['antenna_numbers']),
+                         set([24, 25, 26, 27, 52, 53, 54, 55, 84, 85, 86, 87]))
 
 
 if __name__ == '__main__':
