5922555886064dd2c41acdc493af34f78cc07385
David DeBoer <ddeboer@berkeley.edu>
2017-11-01 10:09:01 -0700

    minor cleanup

--- scripts/parts.py
+++ scripts/parts.py
@@ -18,11 +18,11 @@ if __name__ == '__main__':
                                                    check_rev, overlap_check.  'info' for more.", default='part_info')
     # set values for 'action' to use
     parser.add_argument('-p', '--hpn', help="Part number, csv-list (required). [None]", default=None)
-    parser.add_argument('-r', '--revision', help="Specify revision or last/active/full/all for hpn.  [LAST]", default='LAST')
+    parser.add_argument('-r', '--revision', help="Specify revision or last/active/full/all for hpn.  [ACTIVE]", default='ACTIVE')
     parser.add_argument('-e', '--exact-match', help="Force exact matches on part numbers, not beginning N char. [False]",
                         dest='exact_match', action='store_true')
     parser.add_argument('--check-rev', help="Revision type to check against. [FULL]", dest='check_rev', default='FULL')
-    parser.add_argument('--show-state', help="Show only the 'full', active' or 'all' parts [active]", dest='show_state', default='active')
+    parser.add_argument('--show-state', help="Show only the 'active' or 'all' parts [active]", dest='show_state', default='active')
     cm_utils.add_date_time_args(parser)
 
     args = parser.parse_args()
@@ -43,8 +43,8 @@ if __name__ == '__main__':
 
         Args needing values (or defaulted):
             -p/--hpn:  part name (required)
-            -r/--revision:  revision (particular/last/active/full/all) [LAST]
-            --show-state:  show 'full', 'active' or 'all' parts [ACTIVE]
+            -r/--revision:  revision (particular/last/active/full/all) [ACTIVE]
+            --show-state:  show 'active' or 'all' parts [ACTIVE]
 
         Args that are flags
             -e/--exact-match:  match part number exactly, or specify first characters [False]
@@ -63,32 +63,47 @@ if __name__ == '__main__':
 
     # Process
     if action_tag == 'pa':  # part_info
-        part_dossier = handling.get_part_dossier(hpn_list=args.hpn, rev=args.revision,
-                                                 at_date=date_query, exact_match=args.exact_match)
-        handling.show_parts(part_dossier, verbosity=args.verbosity)
+        if args.hpn is not None:
+            part_dossier = handling.get_part_dossier(hpn_list=args.hpn, rev=args.revision,
+                                                     at_date=date_query, exact_match=args.exact_match)
+            handling.show_parts(part_dossier, verbosity=args.verbosity)
+        else:
+            print("Need to supply part(s)")
 
     elif action_tag == 'co':  # connection_info
-        connection_dossier = handling.get_connection_dossier(
-            hpn_list=args.hpn, rev=args.revision, port=args.port,
-            at_date=date_query, exact_match=args.exact_match)
-        already_shown = handling.show_connections(connection_dossier, verbosity=args.verbosity)
-        handling.show_other_connections(connection_dossier, already_shown)
+        if args.hpn is not None:
+            connection_dossier = handling.get_connection_dossier(
+                hpn_list=args.hpn, rev=args.revision, port=args.port,
+                at_date=date_query, exact_match=args.exact_match)
+            already_shown = handling.show_connections(connection_dossier, verbosity=args.verbosity)
+            handling.show_other_connections(connection_dossier, already_shown)
+        else:
+            print("Need to supply part(s)")
 
     elif action_tag == 'ty':  # types of parts
         part_type_dict = handling.get_part_types(date_query)
         handling.show_part_types()
 
     elif action_tag == 're':  # revisions
-        for hpn in args.hpn:
-            rev_ret = cm_handling.cmpr.get_revisions_of_type(hpn, args.revision, date_query, session)
-            cm_handling.cmpr.show_revisions(rev_ret)
+        if args.hpn is not None:
+            for hpn in args.hpn:
+                rev_ret = cm_handling.cmpr.get_revisions_of_type(hpn, args.revision, date_query, session)
+                cm_handling.cmpr.show_revisions(rev_ret)
+        else:
+            print("Need to supply parts(s)")
 
     elif action_tag == 'ch':  # check revisions
-        for hpn in args.hpn:
-            r = cm_handling.cmpr.check_rev(hpn, args.revision, args.check_rev, date_query, session)
-            rrr = '' if r else ' not'
-            print("{} rev {} is{} {}".format(hpn, args.revision, rrr, args.check_rev))
+        if args.hpn is not None:
+            for hpn in args.hpn:
+                r = cm_handling.cmpr.check_rev(hpn, args.revision, args.check_rev, date_query, session)
+                rrr = '' if r else ' not'
+                print("{} rev {} is{} {}".format(hpn, args.revision, rrr, args.check_rev))
+        else:
+            print("Need to supply part(s)")
 
     elif action_tag == 'ov':  # overlapping revisions
-        for hpn in args.hpn:
-            cm_handling.cmpr.check_part_for_overlapping_revisions(hpn, session)
+        if args.hpn is not None:
+            for hpn in args.hpn:
+                cm_handling.cmpr.check_part_for_overlapping_revisions(hpn, session)
+        else:
+            print("Need to supply part(s)")
