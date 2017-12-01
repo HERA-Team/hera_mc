48d687ff6b2de8dba37f9d9a727d4edb984ea35b
David DeBoer <ddeboer@berkeley.edu>
2017-12-01 10:54:32 -0800

    playing with weather...

--- scripts/mc_wx.py
+++ scripts/mc_wx.py
@@ -10,11 +10,11 @@ from __future__ import absolute_import, division, print_function
 
 from hera_mc import cm_utils, weather, mc
 
-wx_options = ['wind_speed', 'wind_gust', 'wind_direction', 'humidity', 'pressure', 'temperature', 'rain']
+# wx_options = ['wind_speed', 'wind_gust', 'wind_direction', 'humidity', 'pressure', 'temperature', 'rain']
+wx_options = ['wind_gust', 'wind_direction', 'humidity', 'pressure', 'temperature', 'rain']
 if __name__ == '__main__':
     parser = mc.get_mc_argument_parser()
-    parser.add_argument('-v', '--variables', help="Part number, csv-list (required). [None]",
-                        choices=wx_options, default='temperature')
+    parser.add_argument('-v', '--variables', help="Variables csv-list. [all]", choices=wx_options, default='all')
     parser.add_argument('--start-date', dest='start_date', help="Start date YYYY/MM/DD", default=None)
     parser.add_argument('--start-time', dest='start_time', help="Start time in HH:MM", default='17:00')
     parser.add_argument('--stop-date', dest='stop_date', help="Stop date YYYY/MM/DD", default=None)
@@ -22,10 +22,13 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    variables = cm_utils.listify(args.variables)
+    if args.variables.lower() == 'all':
+        variables = wx_options
+    else:
+        variables = cm_utils.listify(args.variables)
     start_time = cm_utils.get_astropytime(args.start_date, args.start_time)
     stop_time = cm_utils.get_astropytime(args.stop_date, args.stop_time)
 
     wx = weather.create_from_sensors(start_time, stop_time, variables)
     for w in wx:
-        print(w.time, w.value)
+        print(w.variable, w.time, w.value)
