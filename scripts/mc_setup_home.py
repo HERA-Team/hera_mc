cbfbe68395d84595aaa2da0b6c4163473403b90c
David DeBoer <ddeboer@berkeley.edu>
2018-01-18 09:19:15 -0800

    adding mcsetup script

---
+++ scripts/mc_setup_home.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2018 the HERA Collaboration
+# Licensed under the 2-clause BSD license.
+
+"""
+Script to read configuration management csv files and initialize tables.
+"""
+
+from hera_mc import mc
+
+parser = mc.get_mc_argument_parser()
+parser.add_argument('--maindb', help="user-generated key to change from main db [False]",
+                    default=False)
+parser.add_argument('--tables', help="name of table for which to initialize or 'all' ['all']",
+                    default='all')
+parser.add_argument('--base', help="use base set of initialization data files [False]",
+                    action='store_true')
+args = parser.parse_args()
+
+cm_transfer.initialize_db_from_csv(tables=args.tables, base=args.base,
+                                   maindb=args.maindb)
