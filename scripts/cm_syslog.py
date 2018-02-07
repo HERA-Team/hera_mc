8b20a4cdfce167941443d1aed2afaa37abeaa57e
David DeBoer <ddeboer@berkeley.edu>
2018-02-07 14:05:00 -0800

    geneRALly better

--- scripts/cm_syslog.py
+++ scripts/cm_syslog.py
@@ -16,7 +16,7 @@ SYSTEM = 'System'  # word used in part_info as hpn for these comments
 
 if __name__ == '__main__':
     parser = mc.get_mc_argument_parser()
-    parser.add_argument('-k', '--keyword', help="Keyword for comments [general]", default='geneRAL')
+    parser.add_argument('-k', '--keyword', help="Keyword for comments [defaults = write: 'general', --view: 'all']", default=None)
     parser.add_argument('-c', '--comment', help="Comment on part", default=None)
     parser.add_argument('-l', '--library_file', help="Library filename/URL", default=None)
     parser.add_argument('-v', '--view', help='View all (or just keyword, if supplied) system comments', action='store_true')
@@ -25,12 +25,13 @@ if __name__ == '__main__':
 
     # Pre-process some args
     if args.view:
-        if args.keyword == 'geneRAL':
-            args.keyword = 'ALL'
+        if args.keyword is None:
+            args.keyword = 'all'
     else:
+        if args.keyword is None:
+            args.keyword = 'general'
         if args.comment is None:
             args.comment = raw_input('Comment:  ')
-    args.keyword = args.keyword.lower()
     at_date = cm_utils.get_astropytime(args.date, args.time)
 
     db = mc.connect_to_mc_db(args)
