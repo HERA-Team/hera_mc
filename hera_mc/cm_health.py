804086353ff169bafad5e400e16a5fd5415433f6
David DeBoer <ddeboer@berkeley.edu>
2018-02-14 11:02:41 -0800

    addressed pkgw comments

--- hera_mc/cm_health.py
+++ hera_mc/cm_health.py
@@ -2,7 +2,7 @@
 # Copyright 2018 the HERA Collaboration
 # Licensed under the 2-clause BSD license.
 
-"""Functions to handle various cm table health checks.
+"""Functions to handle various cm table "health" checks.
 
 """
 
@@ -20,6 +20,15 @@ class RevisionError(Exception):
         super(RevisionError, self).__init__(message)
 
 
+def check_for_overlap(start_i, stop_i, start_j, stop_j):
+    if start_j <= start_i:
+        if stop_j > start_i:
+            return True
+    elif start_j <= stop_i:
+            return True
+    return False
+
+
 class Connections:
     def __init__(self, session=None):
         if session is None:
@@ -29,9 +38,24 @@ class Connections:
             self.session = session
         self.conndict = None
 
-    def get_conndict(self, session=None):
+    def ensure_conndict(self):
+        """
+        If not already set, writes the class variables:
+        conndict - dictionary - conndict[k] = [[start, stop], []...]
+            Dictionary of connections keyed as:
+            upstream_part:rev:output_port:downstream_part:rev:input_port
+            with the items being the start/stop times of that connection.
+            So all entries will have at least one pair.
+        multiple - set - {key1, ...}
+            Set of connections keys with more than one pair, so there is
+            the potential for conflicting times.
+        num_connections:
+            Integer of the total number of connections in database.
+        """
+        if self.conndict is not None:
+            return
         self.conndict = {}
-        self.multiples = []
+        self.multiples = set()
         self.num_connections = 0
         for conn in self.session.query(PC.Connections).all():
             self.num_connections += 1
@@ -40,67 +64,75 @@ class Connections:
             connection = [x.lower() for x in connection]
             k = ':'.join(connection)
             if k in self.conndict.keys():
-                self.conndict[k].append([conn.start_gpstime, conn.stop_gpstime])
-                if k not in self.multiples:
-                    self.multiples.append(k)
-            else:
-                self.conndict[k] = [[conn.start_gpstime, conn.stop_gpstime]]
+                self.multiples.add(k)  # only add to multiples if there is more than one.
+            self.conndict.setdefault(k, []).append([cm_utils.get_astropytime(conn.start_gpstime),
+                                                    cm_utils.get_stopdate(conn.stop_gpstime)])
 
     def check_for_duplicate_connections(self):
-        if self.conndict is None:
-            self.get_conndict()
+        """
+        Checks all of the self.multiple keys to see if any of them overlap in time.
+        If they do, it is a conflicting duplicate connection.
+        Writes a class variable:
+        duplicates - list - [[key, i, j], ...]
+            List which keeps triplets of duplication information as the key and the
+            indices of the conflicting times.
+
+        Returns the number of duplicates found.
+        """
+        self.ensure_conndict()
         if len(self.multiples) == 0:
             print("No duplications found.")
             return 0
-        duplicates = []
+        self.duplicates = []
         for k in self.multiples:
-            for i, t in enumerate(self.conndict[k]):
+            for i in range(len(self.conndict[k])):
                 for j in range(i):
-                    start_i = cm_utils.get_astropytime(self.conndict[k][i][0])
-                    stop_i = cm_utils.get_stopdate(self.conndict[k][i][1])
-                    start_j = cm_utils.get_astropytime(self.conndict[k][j][0])
-                    stop_j = cm_utils.get_stopdate(self.conndict[k][j][1])
-                    print("CM_H:  ", k)
-                    if start_j <= start_i:
-                        if stop_j > start_i:
-                            duplicates.append([k, i, j])
-                    else:
-                        if start_j <= stop_i:
-                            duplicates.append([k, i, j])
-        if len(duplicates):
-            print('{} duplications found.'.format(len(duplicates)))
-            for d in duplicates:
-                k = d[0]
-                start_i = cm_utils.get_astropytime(self.conndict[k][i][0])
-                stop_i = cm_utils.get_astropytime(self.conndict[k][i][1])
-                start_j = cm_utils.get_astropytime(self.conndict[k][j][0])
-                stop_j = cm_utils.get_astropytime(self.conndict[k][j][1])
+                    start_i = self.conndict[k][i][0]
+                    stop_i = self.conndict[k][i][1]
+                    start_j = self.conndict[k][j][0]
+                    stop_j = self.conndict[k][j][1]
+                    if check_for_overlap(start_i, stop_i, start_j, stop_j):
+                        self.duplicates.append([k, i, j])
+        if len(self.duplicates):
+            print('{} duplications found.'.format(len(self.duplicates)))
+            for d in self.duplicates:
+                start_i = self.conndict[d[0]][d[1]][0]
+                stop_i = self.conndict[d[0]][d[1]][1]
+                start_j = self.conndict[d[0]][d[2]][0]
+                stop_j = self.conndict[d[0]][d[2]][1]
                 print('\t{} <1>{}-{}  <2>{}-{}'.format(k, start_i, stop_i, start_j, stop_j))
         else:
             print('No duplications found.')
         print('Out of {} connections checked.'.format(self.num_connections))
-        return len(duplicates)
+        return len(self.duplicates)
 
     def check_for_existing_connection(self, connection, at_date='now'):
+        """
+        Checks whether the provided connection is already set up for the at_date provided.
+
+        Parameters:
+        ------------
+        connection:  connection list [upstream, rev, output_port, downstream, rev, input_port]
+        at_date:  date for the connection to be made
+        """
+
         at_date = cm_utils.get_astropytime(at_date)
         connection = [x.lower() for x in connection]
         k = ':'.join(connection)
-        if self.conndict is None:
-            self.get_conndict()
+        self.ensure_conndict()
         if k not in self.conndict.keys():
             return False
         for t in self.conndict[k]:
-            start_t = cm_utils.get_astropytime(t[0])
-            stop_t = cm_utils.get_astropytime(t[1])
-            if cm_utils.is_active(at_date, start_t, stop_t):
-                print('Duplicate found for connection {} at {}:  {} - {}'.format(k, at_date, start_t, stop_t))
+            if cm_utils.is_active(at_date, t[0], t[1]):
+                print('Connection {} is already made for {}  ({} - {})'.format(k, at_date, t[0], t[1]))
                 return True
         return False
 
 
 def check_part_for_overlapping_revisions(hpn, session=None):
     """
-    Checks hpn for parts that overlap in time.  Should be none.
+    Checks hpn for parts that overlap in time.  They are allowed, but
+    may also signal unwanted behavior.
 
     Returns list of overlapping part revisions
 
@@ -111,16 +143,15 @@ def check_part_for_overlapping_revisions(hpn, session=None):
 
     overlap = []
     revisions = cm_revisions.get_all_revisions(hpn, session)
-    for i, r1 in enumerate(revisions):
-        for j, r2 in enumerate(revisions):
-            if i >= j:
-                continue
-            first_one = i if r1.started <= r2.started else j
-            second_one = i if r1.started > r2.started else j
-            if revisions[first_one].ended is None:
-                revisions[first_one].ended = cm_utils.get_astropytime('>')
-            if revisions[second_one].started > revisions[first_one].ended:
-                overlap.append([r1, r2])
+    for i in range(len(revisions)):
+        for j in range(i):
+            start_i = revisions[i].started
+            stop_i = cm_utils.get_stopdate(revisions[i].ended)
+            start_j = revisions[j].started
+            stop_j = cm_utils.get_stopdate(revisions[j].ended)
+            if check_for_overlap(start_i, stop_i, start_j, stop_j):
+                self.overlap.append([revisions[i], revisions[j]])
+
     if len(overlap) > 0:
         overlapping_revs_in_single_list = []
         for ol in overlap:
