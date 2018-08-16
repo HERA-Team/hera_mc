a9f327d5c6374716a22916988f455d1e6a8c1b0d
Bryna Hazelton <bryna.hazelton@gmail.com>
2018-08-16 10:59:35 -0700

    add debugging prints

--- hera_mc/tests/test_geo_location.py
+++ hera_mc/tests/test_geo_location.py
@@ -25,8 +25,11 @@ class TestGeo(TestHERAMC):
         self.h = geo_handling.Handling(self.test_session, testing=True)
 
     def test_cofa(self):
+        station_types = self.h.get_station_types()
+        print(station_types)
+        self.assertTrue('cofa' in station_types.lower())
+
         cofa = self.h.cofa()[0]
-        self.assertTrue('cofa' in cofa.station_name.lower())
 
         # test that function works the same as method
         cofa_func = geo_handling.cofa(session=self.test_session)[0]
