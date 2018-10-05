c0c8909249eefb6fb366d2daa6289e891ef738e8
ddeboer <ddeboer@berkeley.edu>
2018-10-04 21:30:15 -0700

    added tests

--- hera_mc/tests/test_sys_handling.py
+++ hera_mc/tests/test_sys_handling.py
@@ -153,7 +153,7 @@ class TestSys(TestHERAMC):
 
     def test_system_comments(self):
         comments = self.sys_h.system_comments()
-        self.assertEqual(comments[0], 'N')
+        self.assertEqual(comments[1], 'xK')
 
 
 if __name__ == '__main__':
