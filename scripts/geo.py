a47c62129e84fed66f9f50baff1e134a4e08e3e2
ddeboer <ddeboer@berkeley.edu>
2018-11-05 12:12:03 -0800

    added hide bg

--- scripts/geo.py
+++ scripts/geo.py
@@ -14,9 +14,10 @@ from hera_mc import mc, geo_handling, cm_utils, part_connect
 
 if __name__ == '__main__':
     parser = mc.get_mc_argument_parser()
-    parser.add_argument('action', nargs='?', help="Actions are:  geo, cofa, since", default='geo')
+    parser.add_argument('action', nargs='?', help="Actions are:  g[eo], c[ofa], s[ince]", default='geo')
     parser.add_argument('-p', '--position', help="Position (i.e. station) name", default=None)
     parser.add_argument('-g', '--graph', help="Graph station types [False]", action='store_true')
+    parser.add_argument('--hide-bg', dest = 'background', help="Don't plot the background stations", action='store_false')
     cm_utils.add_date_time_args(parser)
     parser.add_argument('-x', '--xgraph', help="X-axis of graph. [E]",
                         choices=['N', 'n', 'E', 'e', 'Z', 'z'], default='E')
@@ -30,7 +31,7 @@ if __name__ == '__main__':
                         help="Label by station_name (name), ant_num (num) or serial_num (ser) or false [num]",
                         choices=['name', 'num', 'ser', 'false'], default='num')
     args = parser.parse_args()
-    args.action = args.action.lower()[:3]
+    args.action = args.action.lower()
 
     allowed_string_station_type_args = ['default', 'all']
 
@@ -46,7 +47,7 @@ if __name__ == '__main__':
     xgraph = args.xgraph.upper()
     ygraph = args.ygraph.upper()
     show_state = args.show_state.lower()
-    if args.action == 'sin':
+    if args.action.startswith('s'):
         cutoff = at_date
         at_date = cm_utils.get_astropytime('now')
 
@@ -56,24 +57,24 @@ if __name__ == '__main__':
     G = geo_handling.Handling(session)
 
     # If args.graph is set, you will always have this "background"
-    if args.graph:
+    if args.graph and args.background:
         show_fig = G.plot_station_types(query_date=at_date, station_types_to_use=args.station_types,
                                         xgraph=xgraph, ygraph=ygraph,
                                         show_state=show_state, show_label=args.show_label)
-    # Process action.  Actions are:  geo, cofa, corr, since, info
-    if args.action == 'geo' and args.position is not None:
+    # Process action.  Actions are:  geo, cofa, since
+    if args.action.startswith('g') and args.position is not None:
         located = G.get_location(args.position, at_date)
         G.print_loc_info(located)
         if args.graph and len(located) > 0:
             G.plot_stations(args.position, at_date, xgraph=xgraph, ygraph=ygraph, show_label=args.show_label,
                             marker_color='k', marker_shape='*', marker_size=14)
-    elif args.action == 'cof':
+    elif args.action.startswith('c'):
         cofa = G.cofa()
         G.print_loc_info(cofa)
         if args.graph:
             G.plot_stations([cofa[0].station_name], at_date, xgraph=xgraph, ygraph=ygraph, show_label='name',
                             marker_color='k', marker_shape='*', marker_size=14)
-    elif args.action == 'sin':
+    elif args.action.startswith('s'):
         new_antennas = G.get_ants_installed_since(cutoff, args.station_types)
         print("{} new antennas since {}".format(len(new_antennas), cutoff))
         if len(new_antennas) > 0:
