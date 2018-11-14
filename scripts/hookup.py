9c616f138cc2ea525ec296c8d619bde5721446ec
ddeboer <ddeboer@berkeley.edu>
2018-11-14 08:26:11 -0800

    changed hookup default parameter

--- scripts/hookup.py
+++ scripts/hookup.py
@@ -14,11 +14,10 @@ from hera_mc import cm_hookup, cm_utils, mc
 
 if __name__ == '__main__':
     parser = mc.get_mc_argument_parser()
-    parser.add_argument('-p', '--hpn', help="Part number, csv-list (required). [None]", default=None)
+    parser.add_argument('-p', '--hpn', help="Part number, csv-list or default. (default)", default='default')
     parser.add_argument('-r', '--revision', help="Specify revision or last/active/full/all for hpn.  [LAST]", default='LAST')
     parser.add_argument('-e', '--exact-match', help="Force exact matches on part numbers, not beginning N char. [False]",
                         dest='exact_match', action='store_true')
-    parser.add_argument('-q', '--quick', help="Shortcut to show a subset of cols and correlator level", action='store_true')
     parser.add_argument('-f', '--force-new', dest='force_new', help="Force it to write a new hookup file.", action='store_true')
     parser.add_argument('-c', '--cache-info', help="Shows information about the hookup cache file.", dest='cache_info', action='store_true')
     parser.add_argument('--force-specific', dest='force_specific', help="Force db use", action='store_true')
@@ -38,12 +37,10 @@ if __name__ == '__main__':
 
     # Pre-process the args
     args.hookup_cols = cm_utils.listify(args.hookup_cols)
-    if args.quick:
-        if args.hpn is None:
-            args.hpn = ['HH']
-        args.levels = True
-        args.hookup_cols = ['station', 'front-end', 'post-amp', 'snap', 'node']
-    args.hpn = cm_utils.listify(args.hpn)
+    if args.hpn == 'default':
+        args.hpn = cm_utils.default_station_prefixes
+    else:
+        args.hpn = cm_utils.listify(args.hpn)
     # Start session
     db = mc.connect_to_mc_db(args)
     session = db.sessionmaker()
