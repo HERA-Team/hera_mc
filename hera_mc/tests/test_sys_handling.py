a3d57f75116eb37643994fede48bffaec679eccd
ddeboer <ddeboer@berkeley.edu>
2019-03-23 21:25:27 -0700

    got annoying last line

--- hera_mc/tests/test_sys_handling.py
+++ hera_mc/tests/test_sys_handling.py
@@ -113,7 +113,7 @@ class TestSys(TestHERAMC):
         xxx = cm_sysdef.get_port_pols_to_do(part, port_query='e')
         self.assertEqual(xxx, None)
         part.hpn = 'doorknob'
-        xxx = cm_sysdef.get_port_pols_to_do(part, port_query='all')
+        xxx = cm_sysdef.get_port_pols_to_do(part, port_query='pol')
         self.assertTrue('e' in xxx)
         xxx = cm_sysdef.get_port_pols_to_do(part, port_query='e')
         self.assertTrue('e' in xxx)
