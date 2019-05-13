b3a520b1ab055fc6e47760929c6efd383a13f7f9
ddeboer <ddeboer@berkeley.edu>
2019-05-12 22:17:10 -0700

    added fem psu port and corrected mc_publish_summary

--- scripts/mc_publish_summary.py
+++ scripts/mc_publish_summary.py
@@ -9,7 +9,7 @@ If not on qmaster, it just writes the html file.
 """
 from __future__ import absolute_import, division, print_function
 
-from hera_mc import mc, cm_utils, sys_handling
+from hera_mc import mc, cm_utils, cm_sysutils
 
 if __name__ == '__main__':
     default_hookup_cols = ['station', 'feed', 'front-end', 'post-amp', 'snap', 'node']
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--hpn', help="Part number, csv-list or [default]", default='default')
     parser.add_argument('-r', '--revision', help="Specify revision or last/active/full/all for hpn.  [ACTIVE]", default='ACTIVE')
     parser.add_argument('-e', '--exact-match', help="Force exact matches on part numbers, not beginning N char. [False]",
-                        dest='exact_m atch', action='store_true')
+                        dest='exact_match', action='store_true')
     parser.add_argument('-f', '--force-new-cache', dest='force_new_cache', help="Force it to write a new hookup cache.", action='store_false')
     parser.add_argument('--hookup-cols', help="Specify a subset of parts to show comma-delimited no-space list.",
                         dest='hookup_cols', default=default_hookup_cols)
@@ -33,6 +33,6 @@ if __name__ == '__main__':
     db = mc.connect_to_mc_db(args)
     session = db.sessionmaker()
 
-    system = sys_handling.Handling(session)
+    system = cm_sysutils.Handling(session)
     system.publish_summary(args.hpn, rev=args.revision, exact_match=args.exact_match, hookup_cols=args.hookup_cols,
                            force_new_hookup_dict=args.force_new_cache)
