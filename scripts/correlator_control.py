255faae2a7d58212c9a677eb2fe6b18cf06174a6
Bryna Hazelton <bryna.hazelton@gmail.com>
2019-06-13 13:11:35 -0700

    improve docstrings for correlator command script

--- scripts/correlator_control.py
+++ scripts/correlator_control.py
@@ -14,46 +14,50 @@ from astropy.time import Time
 from hera_mc import mc
 from hera_mc import correlator as corr
 
-valid_commands = list(corr.command_dict.keys())
+valid_commands = list(sorted(corr.command_dict.keys()))
 
 if __name__ == '__main__':
     parser = mc.get_mc_argument_parser()
     parser.description = """Send correlator control commands"""
-    parser.add_argument('command', help="correlator control command. One of: "
-                        + ', '.join(valid_commands))
-    parser.add_argument('--address', help="address for correlator redis", default=None)
-    parser.add_argument('--starttime', help="required if command is 'take_data', "
+    parser.add_argument('command', help="Correlator control command. One of: "
+                        + ', '.join(valid_commands) + '.')
+    parser.add_argument('--address', help="Address for correlator redis. The "
+                        "default is used if unspecified.", default=None)
+    parser.add_argument('--starttime', help="Required if command is 'take_data', "
                         "ignored otherwise. Time to start taking data. "
                         "Format readable by astropy Time object, can specify "
-                        "format and scale separately.", default=None)
-    parser.add_argument('--starttime_format', help="astropy Time object format "
-                        "string for interpreting starttime.", default=None)
-    parser.add_argument('--starttime_scale', help="astropy Time object scale "
-                        "string for interpreting starttime.", default=None)
-    parser.add_argument('--duration', help="required if command is 'take_data', "
-                        "ignored otherwise. Length of time to take data for, in seconds. "
-                        "Float", default=None)
-    parser.add_argument('--tag', help="required if command is 'take_data', "
+                        "format and scale separately (string).", default=None)
+    parser.add_argument('--starttime_format', help="Astropy Time object format "
+                        "string for interpreting starttime (string).", default=None)
+    parser.add_argument('--starttime_scale', help="Astropy Time object scale "
+                        "string for interpreting starttime (string).", default=None)
+    parser.add_argument('--duration', help="Required if command is 'take_data', "
+                        "ignored otherwise. Length of time to take data for, "
+                        "in seconds (float). ", default=None)
+    parser.add_argument('--tag', help="Required if command is 'take_data', "
                         "ignored otherwise. Tag which will end up in data files "
-                        "as a header entry, must be from correlator.tag_list "
-                        "(e.g. 'science', 'engineering').", default=None)
-    parser.add_argument('--acclen_spectra', help="only used if command is 'take_data', "
-                        "ignored otherwise. Accumulation length in spectra. "
-                        "Defaults to a value that produces ~10s integration time."
-                        "Integer", default=None)
-    parser.add_argument('--overwrite_take_data', help="only used if command is 'take_data', "
-                        "ignored otherwise.  If there is already a take data starttime "
-                        "in the future, overwrite it with this command."
-                        "Boolean", default=False)
-    parser.add_argument('--config_file', help="only used if command is 'update_config', "
-                        "ignored otherwise. config file to command the correlator to use."
-                        "String", default=None)
-    parser.add_argument('--dryrun', help="just print the list of CorrelatorControlCommand "
-                        "objects, do not issue the power commands or log them "
-                        "to the database' [False]", action='store_true')
-    parser.add_argument('--testing', help="Testing: do not use anything that "
-                        "requires a connection to the correlator (implies dryrun)' [False]",
-                        action='store_true')
+                        "as a header entry, must be one of: "
+                        + ', '.join(corr.tag_list) + '.', default=None)
+    parser.add_argument('--acclen_spectra', help="Only used if command is 'take_data', "
+                        "ignored otherwise. Accumulation length in spectra, "
+                        "must be an integer and a multiple of 2048. "
+                        "Defaults to a value that produces ~10s integration time. ",
+                        default=None)
+    parser.add_argument('--overwrite_take_data', help="Only used if command is "
+                        "'take_data', ignored otherwise. Option to force an "
+                        "overwrite of the next take data command if there is "
+                        "already one scheduled in the future (boolean).",
+                        default=False)
+    parser.add_argument('--config_file', help="Only used if command is "
+                        "'update_config', ignored otherwise. Config file to "
+                        "command the correlator to use (string).", default=None)
+    parser.add_argument('--dryrun', help="Just print the list of "
+                        "CorrelatorControlCommand (and possibly other) objects, "
+                        "that would be added to the database, do not issue the "
+                        "commands or log them to the database [False]", action='store_true')
+    parser.add_argument('--testing', help="Testing mode: do not use anything that "
+                        "requires a connection to the correlator "
+                        "(implies dryrun) [False]", action='store_true')
 
     args = parser.parse_args()
     db = mc.connect_to_mc_db(args)
