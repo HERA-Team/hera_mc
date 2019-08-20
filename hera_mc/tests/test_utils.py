67eb5981cf1b285f4170a29cab2e09fdd8a96b05
Danny Jacobs <daniel.c.jacobs@asu.edu>
2019-08-19 17:38:15 -0700

    lst scheduler tests

--- hera_mc/tests/test_utils.py
+++ hera_mc/tests/test_utils.py
@@ -1,8 +1,39 @@
 import nose.tools as nt
-
+from astropy.time import Time
 from .. import utils
+sidesec = 365.25/366.25 #length of sidereal second in SI seconds.
 
-
+def test_LSTScheduler_lstbinsize():
+    """
+    test that two bins have the right time separation
+    """
+    LSTbin_size = 10
+    starttime1 = Time('2019-9-19T05:05:05.0', format='isot', scale='utc')
+    scheduletime1, hour1 = utils.LSTScheduler(starttime1, LSTbin_size)
+    starttime2 = Time('2019-9-19T05:05:15.0', format='isot', scale='utc')
+    scheduletime2, hour2 = utils.LSTScheduler(starttime2, LSTbin_size)
+    # print("difference between time bins")
+    # print("start difference (should be 15):",(starttime2-starttime1).datetime.seconds)
+    # print("lst difference between calculated start times (should be 10s):",hour2-hour1)
+    # print("utc difference (should be 9.9ish):",(scheduletime2-scheduletime1).value*24*3600)
+    nt.assert_almost_equal((hour2 - hour1).hour * 3600, LSTbin_size)
+    nt.assert_almost_equal((scheduletime2 - scheduletime1).value * 24 * 3600,
+                           LSTbin_size * sidesec, places=5)
+def test_LSTScheduler_multiday():
+    """
+    test that two bins are at the same LST on different days
+    """
+    LSTbin_size = 10
+    starttime1 = Time('2019-9-19T05:04:00.0', format='isot', scale='utc')
+    scheduletime1, hour1 = utils.LSTScheduler(starttime1, LSTbin_size)
+    # lst is 4 minutes earlier every day
+    starttime2 = Time('2019-9-20T05:00:0.0', format='isot', scale='utc')
+    scheduletime2, hour2 = utils.LSTScheduler(starttime2, LSTbin_size)
+    # print("difference between time bins")
+    # print("start difference (should be 15):",(starttime2-starttime1).datetime.seconds)
+    # print("lst difference between calculated start times (should be 10s):",hour2-hour1)
+    # print("utc difference (should be 9.9ish):",(scheduletime2-scheduletime1).value*24*3600)
+    nt.assert_almost_equal((hour2.hour - hour1.hour)*3600, 0)
 def test_reraise_context():
     with nt.assert_raises(ValueError) as cm:
         try:
