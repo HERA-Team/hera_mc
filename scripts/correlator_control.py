4438f09bf544c276b7d0bb5e9ae525b1ca7231c6
Danny Jacobs <daniel.c.jacobs@asu.edu>
2019-08-20 15:40:16 -0700

    take data in 60s, help

--- scripts/correlator_control.py
+++ scripts/correlator_control.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
                         "(implies dryrun) [False]", action='store_true')
     parser.add_argument('--lstlock', help="Lock observation start to a 16s LST grid"
                         "[True]", action='store_true',default=True)
-    parser.add_argument('--now', help="Take data in 60s."
+    parser.add_argument('--now', help="Take data now."
                         "autofills starttime, starttime_format and starttime_scale",
                         action='store_true')
 
