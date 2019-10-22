a0f88c8bd0b4b3a5e99ab9ea800b44dc1a0c0ca2
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-10-22 07:58:38 -0700

    typo fix 's/hashs/hash`

--- scripts/add_rtp_process_record.py
+++ scripts/add_rtp_process_record.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
             obsid=obsid.gps,
             pipeline_list=args.pipeline_list,
             rtp_git_version=hera_opm_version_info['version'],
-            rtp_git_hash=hera_opm_version_info['git_hashs'],
+            rtp_git_hash=hera_opm_version_info['git_hash'],
             hera_qm_git_hash=hera_qm_version_info['version'],
             hera_qm_git_version=hera_qm_version_info['git_hash'],
             hera_cal_git_hash=hera_cal_version_info['version'],
