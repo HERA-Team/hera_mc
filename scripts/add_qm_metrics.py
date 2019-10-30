88e174c2b9ec54a3f995785b972ac4aaa6db3378
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-10-30 13:37:22 -0700

    remove unused imports in script

--- scripts/add_qm_metrics.py
+++ scripts/add_qm_metrics.py
@@ -8,10 +8,6 @@ Script to add quality metrics file to M&C database.
 """
 from __future__ import absolute_import, division, print_function
 
-import os
-import numpy as np
-import warnings
-
 import hera_mc.mc as mc
 
 parser = mc.get_mc_argument_parser()
