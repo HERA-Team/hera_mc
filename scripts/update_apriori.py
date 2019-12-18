080e0ce196a76eb755c800756f5624c3bde58655
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-12-18 10:45:03 -0700

    add new apriori statuses to the update_apriori script

--- scripts/update_apriori.py
+++ scripts/update_apriori.py
@@ -10,11 +10,26 @@ Script to update the apriori antenna status.
 from hera_mc import mc, cm_utils, cm_partconnect
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = mc.get_mc_argument_parser()
-    parser.add_argument('-p', '--hpn', help="HERA part number")
-    parser.add_argument('-s', '--status', help="New apriori status.",
-                        choices=['passed_checks', 'needs_checking', 'known_bad', 'not_connected'])
+    parser.add_argument("-p", "--hpn", help="HERA part number")
+    parser.add_argument(
+        "-s",
+        "--status",
+        help="New apriori status.",
+        choices=[
+            "dish_maintenance",
+            "dish_ok",
+            "RF_maintenance",
+            "RF_ok",
+            "digital_maintenance",
+            "digital_ok",
+            "passed_checks",
+            "needs_checking",
+            "known_bad",
+            "not_connected",
+        ],
+    )
     cm_utils.add_date_time_args(parser)
     args = parser.parse_args()
 
