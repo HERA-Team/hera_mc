185a35b27237025eff41a2133fc9e85eecd2b835
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-12-31 08:57:05 -0700

    added heraautos alembic revision

---
+++ alembic/versions/68041e36e11b_adding_new_heraautos_table.py
@@ -0,0 +1,33 @@
+"""add HeraAutos table
+
+Revision ID: 68041e36e11b
+Revises: b022867d09e3
+Create Date: 2019-12-30 20:56:04.518300+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '68041e36e11b'
+down_revision = 'b022867d09e3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('hera_autos',
+    sa.Column('time', sa.BigInteger(), nullable=False),
+    sa.Column('antenna_number', sa.Integer(), nullable=False),
+    sa.Column('antenna_feed_pol', sa.String(), nullable=False),
+    sa.Column('measurement_type', sa.SmallInteger(), nullable=False),
+    sa.Column('value', sa.Float(precision='53'), nullable=False),
+    sa.PrimaryKeyConstraint('time', 'antenna_number', 'antenna_feed_pol')
+    )
+    # ### end Alembic commands ###
+
+
+def downgrade():
+    op.drop_table('hera_autos')
+    # ### end Alembic commands ###
