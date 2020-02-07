3004818324bb2b09fcad966f97d761fcf35dd8cd
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2020-02-07 13:12:36 -0700

    first pass at a log listening script

---
+++ scripts/mc_listen_to_corr_logger.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2018 the HERA Collaboration
+# Licensed under the 2-clause BSD license.
+
+"""Gather correlator status info and log them into M&C
+
+"""
+from __future__ import absolute_import, division, print_function
+
+import json
+import redis
+import socket
+
+from astropy.time import Time
+
+from hera_mc import mc
+from hera_mc.correlator import DEFAULT_REDIS_ADDRESS
+
+
+parser = mc.get_mc_argument_parser()
+parser.add_argument(
+    "--redishost",
+    "-r",
+    dest="redishost",
+    default=DEFAULT_REDIS_ADDRESS,
+    help="The redis db hostname"
+)
+parser.add_argument(
+    "--channel",
+    "-c",
+    dest="channel",
+    default="mc-log-channel",
+    help="The redis channel to listen on."
+)
+args = parser.parse_args()
+db = mc.connect_to_mc_db(args)
+
+hostname = socket.gethostname()
+redis_pool = redis.ConnectionPool(host=args.redishost)
+
+while True:
+    try:
+        with db.sessionmaker() as session, redis.Redis(connection_pool=redis_pool) as redis_db:
+            pubsub = redis_db.pubsub()
+
+            if not pubsub.subscribed():
+                pubsub.subscribe(args.channel)
+            # pubsub.listen() will create an infinite generator
+            # that yields messages in our channel
+            for message in pubsub.listen():
+                if (
+                    message["type"] != "subscribe"
+                    and message["channel"] == args.channel
+                    # messages come as byte strings, make sure an error didn't occur
+                    and message["data"].decode() != "UnicodeDecodeError on emit!"
+                ):
+                    message_dict = json.loads(message["data"])
+
+                    session.add_subsystem_error(
+                        Time(message_dict["logtime"], format="unix"),
+                        message_dict["subsystem"],
+                        message_dict["severity"],
+                        message_dict["message"],
+                    )
+
+                    session.add_daemon_status(
+                        "mc_monitor_correlator", hostname, Time.now(), "good"
+                    )
+    except Exception:
+        session.add_daemon_status(
+            "mc_monitor_correlator", hostname, Time.now(), "errored"
+        )
+        continue
