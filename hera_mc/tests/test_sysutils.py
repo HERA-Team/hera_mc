03c3bfd7514cda548e6c8a0a2db74d9657219971
David DeBoer <ddeboer@berkeley.edu>
2020-02-19 09:45:47 -0800

    update cminfo contents

--- hera_mc/tests/test_sysutils.py
+++ hera_mc/tests/test_sysutils.py
@@ -258,9 +258,8 @@ def test_correlator_info(sys_handle):
 
     assert corr_dict['cm_version'] == mc_git_hash
 
-    expected_keys = ['antenna_numbers', 'antenna_names', 'station_types',
-                     'correlator_inputs', 'utm_datum',
-                     'utm_tile', 'antenna_utm_eastings', 'antenna_alt',
+    expected_keys = ['antenna_numbers', 'antenna_names', 'correlator_inputs',
+                     'utm_datum', 'utm_tile', 'antenna_utm_eastings', 'antenna_alt',
                      'antenna_utm_northings', 'antenna_positions',
                      'cm_version', 'cofa_lat', 'cofa_lon', 'cofa_alt',
                      'cofa_X', 'cofa_Y', 'cofa_Z']
