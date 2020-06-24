933cd931382c58d8bd942e97ed7bfe818bf0731c
Paul La Plante <plaplant@sas.upenn.edu>
2020-06-24 07:07:08 -0700

    Remove aipy import

--- scripts/mc_add_observation.py
+++ scripts/mc_add_observation.py
@@ -7,7 +7,6 @@ import os
 import numpy as np
 from astropy.time import Time
 
-import aipy
 from pyuvdata import UVData
 
 from hera_mc import mc
@@ -21,24 +20,13 @@ db = mc.connect_to_mc_db(args)
 
 
 for uvfile in args.files:
-    if os.path.isdir(uvfile):
-        # assume it's a miriad file
-        # we need aipy to read the stopt and duration because everything is awful
-        # but first tell AIPY we're allowed to read these variables
-        aipy.miriad.itemtable['stopt'] = 'd'
-        aipy.miriad.itemtable['duration'] = 'd'
-        UV = aipy.miriad.UV(uvfile)
-        obsid = UV['obsid']
-        stoptime = Time(UV['stopt'], scale='utc', format='gps')
-        starttime = Time(UV['startt'], scale='utc', format='gps')
-    else:
-        # assume it's uvh5
-        uv = UVData()
-        uv.read_uvh5(uvfile, read_data=False)
-        times = np.unique(uv.time_array)
-        starttime = Time(times[0], scale='utc', format='jd')
-        stoptime = Time(times[-1], scale='utc', format='jd')
-        obsid = int(np.floor(starttime.gps))
+    # assume our data file is uvh5
+    uv = UVData()
+    uv.read_uvh5(uvfile, read_data=False)
+    times = np.unique(uv.time_array)
+    starttime = Time(times[0], scale='utc', format='jd')
+    stoptime = Time(times[-1], scale='utc', format='jd')
+    obsid = int(np.floor(starttime.gps))
 
     with db.sessionmaker() as session:
         obs = session.get_obs(obsid)
