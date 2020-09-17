4842b017f924c79882377401dba7a07ffa1ef2ee
David DeBoer <ddeboer@berkeley.edu>
2020-09-17 15:29:59 -0700

    added time threshold

--- hera_mc/watch_dog.py
+++ hera_mc/watch_dog.py
@@ -13,16 +13,22 @@ def read_forward_list():
             fwd.append(line.strip())
 
 
-def node_temperature(at_date=None, threshold=45, To=None, session=None):
+def node_temperature(at_date=None, temp_threshold=45.0, time_threshold=1.0,
+                     To=None, session=None):
     """
     Check node for over-temperature.
+
+    This checks all of the temperature sensors in active nodes, and sends an email
+    if any sensor exceeds that provided threshold.
 
     Parameters
     ----------
     at_date : anything understandable by get_astropytime
-        If None, use values at latest time.  If valid Time, use nearest value after.
-    threshold : float
+        If None, use values at latest time.  If valid Time, use nearest value before.
+    temp_threshold : float
         Threshold temperature in Celsius
+    time_threshold : float
+        Time in days for "current" values.
     To : list or None
         List of e-mail addresses.  If None, uses the addresses in ~/.forward
     session : session object or None
@@ -35,29 +41,32 @@ def node_temperature(at_date=None, threshold=45, To=None, session=None):
         db = mc.connect_to_mc_db(None)
         session = db.sessionmaker()
 
-    at_date = cm_utils.get_astropytime(at_date)
+    if at_date is None:
+        use_last = True
+        at_date = cm_utils.get_astropytime('now')
+    else:
+        use_last = False
+        at_date = cm_utils.get_astropytime(at_date)
+    gps_time = at_date.gps
     active_parts = cm_active.ActiveData()
     active_parts.load_parts(at_date)
     active_nodes = [int(cm_utils.split_part_key(key)[0][1:])
                     for key in active_parts.get_hptype('node')]
 
     active_temps = {}
-    msg_header = 'Over-temperature ({} C)'.format(threshold)
+    msg_header = 'Over-temperature (>{} C)'.format(temp_threshold)
     msg = '{}'.format(msg_header)
     for node_num in active_nodes:
-        if at_date is None:
+        if use_last:
             nds = (session.query(node.NodeSensor)
                    .filter(node.NodeSensor.node == node_num)
                    .order_by(node.NodeSensor.time.desc()).first())
-            if nds is None:
-                continue
         else:
-            gps_time = at_date.gps
             nds = (session.query(node.NodeSensor)
-                   .filter(node.NodeSensor.time >= gps_time & node.NodeSensor.node == node_num)
-                   .order_by(node.NodeSensor.time).first())
-            if nds is None:
-                continue
+                   .filter(node.NodeSensor.time < gps_time & node.NodeSensor.node == node_num)
+                   .order_by(node.NodeSensor.time.desc()).first())
+        if nds is None or (gps_time - nds.time) / 86400.0 > time_threshold:
+            continue
         active_temps[node_num] = []
         for _t in [nds.top_sensor_temp, nds.middle_sensor_temp,
                    nds.bottom_sensor_temp, nds.humidity_sensor_temp]:
@@ -66,7 +75,7 @@ def node_temperature(at_date=None, threshold=45, To=None, session=None):
             else:
                 active_temps[node_num].append(_t)
         highest_temp = max(active_temps[node_num])
-        if highest_temp > threshold:
+        if highest_temp > temp_threshold:
             msg += "\t{:02d}:  {}\n".format(node_num, ', '.join(active_temps[node_num]))
     if msg != msg_header:
         import smtplib
