a63580c474e427248f1c600ce5db900c16ea2020
Dave DeBoer <ddeboer@berkeley.edu>
2022-01-21 15:09:13 -0800

    get codecov

--- hera_mc/tests/test_correlator.py
+++ hera_mc/tests/test_correlator.py
@@ -2341,8 +2341,10 @@ def test_add_antenna_status(mcsession):
     histogram = [0, 3, 6, 10, 12, 8, 4, 0]
     pam_id_list = [112, 217, 32, 59, 1, 0, 0, 14]
     pam_id = ":".join([str(i) for i in pam_id_list])
+    ####
     fem_id_list = [0, 168, 19, 212, 51, 51, 255, 255]
     fem_id = ":".join([str(i) for i in fem_id_list])
+    fem_id = "UNKNOWN"
     test_session.add_antenna_status(
         t1,
         4,
