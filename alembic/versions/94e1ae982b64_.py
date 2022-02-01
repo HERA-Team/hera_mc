8af5510bc363bb450a7cc9b107e194861a95fa59
Dave DeBoer <ddeboer@berkeley.edu>
2022-01-31 17:03:41 -0800

    merge alembic

---
+++ alembic/versions/94e1ae982b64_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 94e1ae982b64
+Revises: c66b6b222bde, d2f22cbd4c5a
+Create Date: 2022-02-01 01:01:45.400770+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "94e1ae982b64"
+down_revision = ("c66b6b222bde", "d2f22cbd4c5a")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
