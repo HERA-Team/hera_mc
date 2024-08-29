056628ac7dfe011d51a659329e7caf8ae2e1e56a
Josh Dillon <jsdillon@berkeley.edu>
2024-08-29 16:35:45 -0700

    also update script

--- scripts/mc_launch_rtp.py
+++ scripts/mc_launch_rtp.py
@@ -71,7 +71,7 @@ ap.add_argument(
     "-c",
     "--workflow_config",
     type=str,
-    default="/home/obs/src/hera_pipelines/pipelines/h7c/rtp/v1/h7c_rtp.toml",
+    default="/home/obs/src/hera_pipelines/pipelines/h8c/rtp/v1/h8c_rtp.toml",
     help="hera_opm configuration to use for workflow",
 )
 ap.add_argument(
