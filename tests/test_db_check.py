46cce80eb2c0300b43dd849fd8397685d044cf3e
Bryna Hazelton <bryna.hazelton@gmail.com>
2026-03-09 15:24:45 -0700

    use more context managers

--- tests/test_db_check.py
+++ tests/test_db_check.py
@@ -80,63 +80,56 @@ def test_validity_pass():
     See database validity check completes when tables and columns are created.
     """
     engine = mc.connect_to_mc_testing_db().engine
-    conn = engine.connect()
-    conn.begin()
 
-    Base, ValidTestModel = gen_test_model()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    try:
-        Base.metadata.drop_all(engine, tables=[ValidTestModel.__table__])
-    except sqlalchemy.exc.NoSuchTableError:
-        pass
+    with engine.connect() as conn:
+        Base, ValidTestModel = gen_test_model()
+        Session = sessionmaker(bind=conn)
+        with Session() as session:
+            try:
+                Base.metadata.drop_all(engine, tables=[ValidTestModel.__table__])
+            except sqlalchemy.exc.NoSuchTableError:
+                pass
 
-    base_is_none = is_valid_database(None, session)
-    assert base_is_none
+            base_is_none = is_valid_database(None, session)
+            assert base_is_none
 
-    Base.metadata.create_all(engine, tables=[ValidTestModel.__table__])
+            Base.metadata.create_all(engine, tables=[ValidTestModel.__table__])
 
-    try:
-        assert is_valid_database(Base, session) is True
-    finally:
-        Base.metadata.drop_all(engine)
-        session.close()
-        conn.close()
+            try:
+                assert is_valid_database(Base, session) is True
+            finally:
+                Base.metadata.drop_all(engine)
 
 
 def test_validity_table_missing():
     """See check fails when there is a missing table"""
     engine = mc.connect_to_mc_testing_db().engine
-    conn = engine.connect()
-    conn.begin()
 
-    Base, ValidTestModel = gen_test_model()
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    with engine.connect() as conn:
+        Base, ValidTestModel = gen_test_model()
 
-    try:
-        Base.metadata.drop_all(engine, tables=[ValidTestModel.__table__])
-    except sqlalchemy.exc.NoSuchTableError:
-        pass
+        Session = sessionmaker(bind=conn)
+        with Session() as session:
+            try:
+                Base.metadata.drop_all(engine, tables=[ValidTestModel.__table__])
+            except sqlalchemy.exc.NoSuchTableError:
+                pass
 
-    assert is_valid_database(Base, session) is False
-    session.close()
-    conn.close()
+            assert is_valid_database(Base, session) is False
 
 
 def test_validity_column_missing():
     """See check fails when there is a missing table"""
     engine = mc.connect_to_mc_testing_db().engine
     with engine.begin() as conn:
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        Base, ValidTestModel = gen_test_model()
-        try:
-            Base.metadata.drop_all(engine, tables=[ValidTestModel.__table__])
-        except sqlalchemy.exc.NoSuchTableError:
-            pass
-        Base.metadata.create_all(engine, tables=[ValidTestModel.__table__])
-        session.close()
+        Session = sessionmaker(bind=conn)
+        with Session() as session:
+            Base, ValidTestModel = gen_test_model()
+            try:
+                Base.metadata.drop_all(engine, tables=[ValidTestModel.__table__])
+            except sqlalchemy.exc.NoSuchTableError:
+                pass
+            Base.metadata.create_all(engine, tables=[ValidTestModel.__table__])
 
         # Delete one of the columns
         conn.execute(text("ALTER TABLE validity_check_test DROP COLUMN id_"))
@@ -144,11 +137,10 @@ def test_validity_column_missing():
     # use a new context manager to make sure there are no open transactions
     # without this it hangs
     with engine.begin() as conn:
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        assert is_valid_database(Base, session) is False
-        Base.metadata.drop_all(engine)
-        session.close()
+        Session = sessionmaker(bind=conn)
+        with Session() as session:
+            assert is_valid_database(Base, session) is False
+            Base.metadata.drop_all(engine)
 
 
 def test_validity_pass_relationship():
@@ -157,30 +149,27 @@ def test_validity_pass_relationship():
     deem them as missing column.
     """
     engine = mc.connect_to_mc_testing_db().engine
-    conn = engine.connect()
-    conn.begin()
+    with engine.connect() as conn:
+        Session = sessionmaker(bind=conn)
+        with Session() as session:
+            Base, RelationTestModel, RelationTestModel2 = gen_relation_models()
+            try:
+                Base.metadata.drop_all(
+                    engine,
+                    tables=[RelationTestModel.__table__, RelationTestModel2.__table__],
+                )
+            except sqlalchemy.exc.NoSuchTableError:
+                pass
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+            Base.metadata.create_all(
+                engine,
+                tables=[RelationTestModel.__table__, RelationTestModel2.__table__],
+            )
 
-    Base, RelationTestModel, RelationTestModel2 = gen_relation_models()
-    try:
-        Base.metadata.drop_all(
-            engine, tables=[RelationTestModel.__table__, RelationTestModel2.__table__]
-        )
-    except sqlalchemy.exc.NoSuchTableError:
-        pass
-
-    Base.metadata.create_all(
-        engine, tables=[RelationTestModel.__table__, RelationTestModel2.__table__]
-    )
-
-    try:
-        assert is_valid_database(Base, session) is True
-    finally:
-        Base.metadata.drop_all(engine)
-        session.close()
-        conn.close()
+            try:
+                assert is_valid_database(Base, session) is True
+            finally:
+                Base.metadata.drop_all(engine)
 
 
 def test_validity_pass_declarative():
@@ -189,26 +178,21 @@ def test_validity_pass_declarative():
     them as missing column.
     """
     engine = mc.connect_to_mc_testing_db().engine
-    conn = engine.connect()
-    conn.begin()
+    with engine.connect() as conn:
+        Session = sessionmaker(bind=conn)
+        with Session() as session:
+            Base, DeclarativeTestModel = gen_declarative()
+            try:
+                Base.metadata.drop_all(engine, tables=[DeclarativeTestModel.__table__])
+            except sqlalchemy.exc.NoSuchTableError:
+                pass
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+            Base.metadata.create_all(engine, tables=[DeclarativeTestModel.__table__])
 
-    Base, DeclarativeTestModel = gen_declarative()
-    try:
-        Base.metadata.drop_all(engine, tables=[DeclarativeTestModel.__table__])
-    except sqlalchemy.exc.NoSuchTableError:
-        pass
-
-    Base.metadata.create_all(engine, tables=[DeclarativeTestModel.__table__])
-
-    try:
-        assert is_valid_database(Base, session) is True
-    finally:
-        Base.metadata.drop_all(engine)
-        session.close()
-        conn.close()
+            try:
+                assert is_valid_database(Base, session) is True
+            finally:
+                Base.metadata.drop_all(engine)
 
 
 def test_check_connection(tmpdir):
